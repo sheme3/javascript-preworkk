9ad270130788cac3b8cec3173b579f96b5e60635
Szymon Hachlowski <hachlowski.szymon@gmail.com>
2022-10-24 18:25:03 +0200

    add js code

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
-var computerMove;
-computerMove = 'kamień';
-var playerMove;
-playerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ' to wygrywasz!');
-
-
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+    computerMove = 'kamień';
+} else if (randomNumber == '2') {
+    computerMove = 'papier';
+} else if (randomNumber == '3') {
+    computerMove = 'Nożyce';
+} else {
+    computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
